69c7fdb96b0adb703e5b7c73d95773f2f05da71f
Abbox Games <abboxgames@Abboxs-MacBook-Air.local>
2023-02-14 22:14:55 +0100

    initial commit

---
+++ src/pages/cmsStyles/CmsStylesPage.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+// import styles from './CmsStyles.styles.css'
+
+interface CmsStylesPageProps { }
+
+const CmsStylesPage = (props: CmsStylesPageProps) => {
+  return (<div className='cmsStyles'>cms styles</div>)
+}
+
+export default CmsStylesPage
---
+++ src/pages/landing/LandingPage.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+interface LandingPageProps { }
+
+const LandingPage = (props: LandingPageProps) => {
+  return (<div className='landing'>landing</div>)
+}
+
+export default LandingPage
